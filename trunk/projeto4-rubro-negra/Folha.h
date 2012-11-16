1e10a5df63666d67731df2f1cd70d7edbb340992
rafa_metallica@hotmail.com <rafa_metallica@hotmail.com@b75d0b59-2147-1d5f-6e3e-15e25e489b9d>
2012-11-16 20:54:37 +0000

    Mensagem de Importação obrigatória

---
+++ trunk/projeto4-rubro-negra/Folha.h
@@ -0,0 +1,15 @@
+#ifndef FOLHA_H
+#define	FOLHA_H
+
+class Folha {
+public:
+    Folha(int valor);
+    int valor;
+    char cor;
+    Folha *folhaDir;
+    Folha *folhaEsq;
+    void setCor(Folha f, char c);
+    char getCor(Folha f);
+};
+
+#endif	/* FOLHA_H */
---
+++ trunk/projeto4-rubro-negra/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include "RubroNegra.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    char *filename = argv[1];
+    FILE *arq = fopen(filename, "r");
+    if (arq == NULL) {
+        cout << "Nao foi possivel abrir o arquivo de entrada." << endl;
+        return 0;
+    }
+    RubroNegra *arvore = new RubroNegra();
+    char comando;
+    int valor = 0;
+    while (!feof(arq)) {
+        fscanf(arq, "%s", &comando);
+        fscanf(arq, "%d", &valor);
+        if (comando == 'i') {
+            cout << "> " << comando << " " << valor << endl;
+            arvore->add(valor);
+            arvore->printTree();
+            cout << endl;
+        }
+        if (comando == 'b') {
+            cout << "> " << comando << " " << valor << endl;
+            arvore->busca(valor);
+            cout << endl;
+        }
+    }
+    fclose(arq);
+    delete arvore;
+    return 0;
+}
