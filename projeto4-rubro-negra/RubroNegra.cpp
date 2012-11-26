ce6e3b11e94babfa21c6723b76fca04aab02152c
rafa_metallica@hotmail.com <rafa_metallica@hotmail.com@b75d0b59-2147-1d5f-6e3e-15e25e489b9d>
2012-11-26 19:29:10 +0000

    Falta arrumar esse fixup

--- projeto4-rubro-negra/RubroNegra.cpp
+++ projeto4-rubro-negra/RubroNegra.cpp
@@ -33,40 +33,45 @@ void RubroNegra::rb_fixup(Folha *f) {
         if (f->pai == f->pai->pai->folhaEsq) { //pai de f é filho esquerdo
             Folha *aux = f->pai->pai->folhaDir; //aux aponta pro tio de f
             if (aux->cor = 'v') { //caso 1: tio vermelho
+                cout << "caso 1: tio vermelho" << endl;
                 aux->pai->cor = 'p';
                 aux->cor = 'p'; //tio vermelho vira preto
                 f->pai->pai->cor = 'v'; //avô vira vermelho
             } else {
                 if (f == f->pai->folhaDir) { //caso 2: f é filho direito
+                    cout << "caso 2: f é filho direito" << endl;
                     f = f->pai;
                     rot_esq(f); //rotaciona à esquerda no pai de f
                 }
                 f->pai->cor = 'p'; //caso 3: f é filho esquerdo
+                cout << "caso 3: f é filho esquerdo" << endl;
                 f->pai->pai->cor = 'v'; //avô vira vermelho
                 rot_dir(f->pai->pai); //rotaciona à direita no avô de f
             }
         } else {
             Folha *aux = f->pai->pai->folhaEsq; //aux aponta pro tio de f
             if (aux->cor = 'v') { //caso 1: tio vermelho
+                cout << "caso 4: tio preto" << endl;
                 aux->pai->cor = 'p';
                 aux->cor = 'p'; //tio vermelho vira preto
                 f->pai->pai->cor = 'v'; //avô vira vermelho
             } else {
                 if (f == f->pai->folhaEsq) { //caso 2: f é filho esquerdo
+                    cout << "caso 5" << endl;
                     f = f->pai;
                     rot_dir(f); //rotaciona à direita no pai de f
                 }
                 f->pai->cor = 'p'; //caso 3: f é filho direito
+                cout << "caso 6" << endl;
                 f->pai->pai->cor = 'v'; //avô vira vermelho
                 rot_esq(f->pai->pai); //rotaciona à esquerda no avô de f
             }
         }
-        this->raiz->cor = 'p';
     }
+    this->raiz->cor = 'p';
 }
 
 void RubroNegra::rot_esq(Folha *f) {
-    cout << "rot esq" << endl;
     Folha *aux = f->folhaDir;
     f->folhaDir = aux->folhaEsq;
     if (aux->folhaEsq != this->nil) {
@@ -81,7 +86,6 @@ void RubroNegra::rot_esq(Folha *f) {
     }
     aux->folhaEsq = f;
     f->pai = aux;
-    cout << "end rot esq" << endl;
 }
 
 void RubroNegra::rot_dir(Folha *f) {
